571c49531783b390cd8d9286ab3855ea4fe70020
Shitaye19 <shitmesk@yahoo.com>
2020-11-01 15:32:02 -0500

    graph saved with ggsave

--- For_loop.R
+++ For_loop.R
@@ -53,6 +53,42 @@ my_plot <-  ggplot(data = gap_to_plot, aes(x = year, y = gdpPercap)) +
 ggsave(filename= "Afghanistan_gdpPercap.png", plot = my_plot)
 
 
+##Create country variable 
+
+cntry<- "Afghanistan"
+
+
+##Create country variable 
+
+cntry<- "Afghanistan"
+
+##filter the country to plot 
+
+gap_to_plot <-  gapminder %>% 
+  
+  filter(country == cntry)
+#plot
+
+my_plot <- ggplot(data = gap_to_plot, aes(x = year, y = gdpPercap))+
+  geom_point()+
+  
+## add title and save 
+  
+  labs(title = paste(cntry, "GDP per capita", sep = " "))
+
+## note: there are many ways to create filenames with paste() or file.path(); we are doing this way for a reason.
+  
+  ggsave(filename = paste(cntry, "_gdpPercap.png", sep = " "), plot = my_plot)
+  
+  
+  
+  
+
+
+
+
+
+
 
 
 
